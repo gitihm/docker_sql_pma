0eb6a8c21c0f0155fc79f3615cc41c922f2fb3d4
air <s5935512030@phuket.psu.ac.th>
2020-03-06 00:33:10 +0700

    add html

---
+++ www/web/precache-manifest.dcadb76f9a21edc404fa0aa451aa2e70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d2f62934aca0da1c0babc7cb83a518",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d64b6ad9706cb79b028e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b0acda8230fb7255044a",
+    "url": "/static/js/2.bfa6e66c.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.bfa6e66c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64b6ad9706cb79b028e",
+    "url": "/static/js/main.05d3a7cd.chunk.js"
+  },
+  {
+    "revision": "d2e3d287abbff0085f2e",
+    "url": "/static/js/runtime-main.9d651b5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
